4aab4dd9561cc54df697687d65b29d34b490a44c
rvkoon <koontchak@gmail.com>
2023-02-19 22:37:55 +0100

    add system to generate components + ui module

--- modules/profile/components/User/User.tsx
+++
@@ -1,5 +0,0 @@
-export interface IUserProps {}
-
-export function User ({}: IUserProps): JSX.Element {
-    return <div></div>
-}
--- modules/profile/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './components';
-export * from './helpers';
-export * from './hooks';
-export * from './services';
-export * from './types';
-export * from './constants';
